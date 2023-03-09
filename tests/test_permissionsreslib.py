aa57719a9529995b88d2ba91392ef3077cb1a2ec
Giuseppe Raffa <giuseppe.raffa@hotmail.com>
2023-03-09 14:59:11 +0000

    Added test function test_iam_old_syntax_multi_services_resources and required test file

--- tests/test_permissionsreslib.py
+++ tests/test_permissionsreslib.py
@@ -81,3 +81,14 @@ def test_multi_services_resources(get_test_files_folder):
     assert perm_identifier_obj.perm_dict['rekognition'] == set(['*'])
     assert perm_identifier_obj.perm_dict['dynamodb'] == \
         set(['Query', 'Scan', 'GetItem', 'PutItem', 'UpdateItem', 'DeleteItem'])
+
+def test_iam_old_syntax_multi_services_resources(get_test_files_folder):
+    test_file = os.path.join(get_test_files_folder, 'serverless_iam_old_syntax_multi_services_resources.yml')
+    with open(test_file, mode='r') as file_obj:
+        extracted_dict = yaml.load(file_obj, Loader=yaml.BaseLoader)
+        perm_identifier_obj = PermissionsIdentifierCls(extracted_dict)
+    assert perm_identifier_obj.get_num_of_services() == 2
+    assert perm_identifier_obj.perm_dict['s3'] == \
+        set(['ListBucket', 'GetObject', 'PutObject'])
+    assert perm_identifier_obj.perm_dict['rds'] == \
+        set(['DownloadCompleteDBLogFile', 'DescribeDBLogFiles'])
