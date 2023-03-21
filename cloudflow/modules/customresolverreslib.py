29fc8be709da276062ea408b89c0e42539ded3da
Giuseppe Raffa <giuseppe.raffa@hotmail.com>
2023-03-21 12:34:45 +0000

    Added recursive call for nested unresolved values and modified returned value when file is detected

--- cloudflow/modules/customresolverreslib.py
+++ cloudflow/modules/customresolverreslib.py
@@ -39,10 +39,14 @@ class YAMLResolverCls:
     def _process_value(self, value):
         """
         Method that recursively processes a value extracted from the YAML file
-        to resolve it.
+        to resolve it. If a value is specified via an external file, the code
+        attempts to resolve the file path only, but it does not process the
+        file (if needed, this can be done by the calling code).
         """
         # Regular expression used to identify unresolved values
         unres_val_reg_exp = re.compile(r'\$\{(.*)\}')
+        # Regular expression used to identify values specified via external files
+        file_detect_reg_exp = re.compile(r'^file\(.*\)')
         search_obj = unres_val_reg_exp.search(value)
         if search_obj is not None:
             extracted_str = search_obj.group(1)
@@ -55,10 +59,21 @@ class YAMLResolverCls:
                 # https://www.serverless.com/framework/docs/guides/parameters
                 if ',' in extracted_str:
                     extracted_str = extracted_str.split(',')[1].strip()
+                # The following code handles the case where the extracted string
+                # contains in turn an unresolved part. A recursive call is used
+                # to fully resolve the extracted string.
+                nested_search_obj = unres_val_reg_exp.search(extracted_str)
+                if nested_search_obj is not None:
+                    extracted_str = extracted_str[:nested_search_obj.start()] + \
+                    self._process_value('${' + nested_search_obj.group(1) + '}') + \
+                    extracted_str[nested_search_obj.end():]
                 # The following code assumes that there are no other occurrences
                 # of ${...} within the extracted string.
                 res_value = self.ref_dict
-                for key in re.sub('^\$?\{?self:', '', extracted_str).split('.', 1):
+                # This cycle is needed because the unresolved value is specified
+                # by using '.' notation, but this cannot be used to traverse
+                # self.ref_dict, which is a standard dictionary.
+                for key in re.sub('^\$?\{?self:', '', extracted_str).split('.'):
                     # The dictionary method get is not used in the following
                     # code, as extracting None when a dictionary key is invalid
                     # breaks the yaml module parser.
@@ -70,7 +85,10 @@ class YAMLResolverCls:
                     res_value + self._process_value(value[search_obj.end():])
             except Exception as e:
                 print(f'--- Value {extracted_str} could not be resolved ---')
-                return value
+                if file_detect_reg_exp.search(extracted_str) is not None:
+                    return extracted_str
+                else:
+                    return value
         else:
             return value
 
@@ -110,4 +128,3 @@ class YAMLResolverCls:
             return [self._visit(child) for child in node.value]
         elif isinstance(node, yaml.MappingNode):
             return {self._visit(key): self._visit(value) for key, value in node.value}
-
