721d84ef7e823a5fd097a5f5bea189b3a329ae5a
Giuseppe Raffa <giuseppe.raffa@hotmail.com>
2023-03-22 11:16:15 +0000

    Module dedicated to managing the execution of the data flow analysis

---
+++ cloudflow/modules/analysisreslib.py
@@ -0,0 +1,11 @@
+# =======
+# Classes
+# =======
+class AnalysisManagerCls:
+    # === Constructor ===
+    def __init__(self, config_obj):
+        self.config_obj = config_obj
+    
+    # === Method ===
+    def perform_analysis(self):
+        print('--- CloudFlow Analysis - Start ---')
