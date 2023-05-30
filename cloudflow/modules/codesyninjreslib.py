3c1b62f3d650718dd4cbe8a5126cfbda464053c2
Giuseppe Raffa <giuseppe.raffa@hotmail.com>
2023-05-30 18:22:41 +0100

    Modified constructor of CodeSynInjManagerCls and added methods to CodeSynthesisInjectionCls

--- cloudflow/modules/codesyninjreslib.py
+++ cloudflow/modules/codesyninjreslib.py
@@ -3,8 +3,30 @@
 # ========================================
 import ast
 import astor
+import collections
 import os
 import yaml
+
+# =========
+# Functions
+# =========
+def get_events_handlers_dict(handlers_dict):
+    """
+    Function that reverses the mapping implemented in the handlers
+    dictionary provided by the HandlersEventsIdentifierCls class
+    (module handlerseventsreslib). Reversing the mapping passed as
+    input argument facilitates the retrieval of the all the handlers
+    triggered by a given event. The returned data structure has
+    service event tuples as keys a sets of handlers as values.
+    """
+    # initialization of the returned data structure
+    events_handlers_dict = collections.defaultdict(set)
+    for handler, service_event_set in handlers_dict.items():
+        # The tuples processed within this nested for cycle will
+        # become the keys of the returned dictionary
+        for service_event_tuple in service_event_set:
+            events_handlers_dict[service_event_tuple].add(handler)
+    return events_handlers_dict
 
 # =======
 # Classes
@@ -20,7 +42,8 @@ class CodeSynInjManagerCls:
     def __init__(self,
                  interf_objs_dict,
                  perm_dict,
-                 handlers_dict):
+                 handlers_dict,
+                 infrastruc_code_dict):
         """
         Class constructor.
         """
@@ -30,6 +53,8 @@ class CodeSynInjManagerCls:
         self.perm_dict = perm_dict
         # Data structure containing handlers-related information
         self.handlers_dict = handlers_dict
+        # Data structure containing the infrastructure code dictionary
+        self.infrastruc_code_dict = infrastruc_code_dict
 
     # === Protected Method ===
     def _get_source_code_info(self):
@@ -68,7 +93,6 @@ class CodeSynInjManagerCls:
         Method that injects synthesized code to simulate a
         synchronous-like execution flow.
         """
-        print('--- Synthesized handler calls are about to be injected ---')
         for sc_file, interf_objs_info_list in self._get_source_code_info():
             print('--- The following file is about to be processed: ---')
             print(f'--- {sc_file} ---')
@@ -80,7 +104,9 @@ class CodeSynInjManagerCls:
                     walker = CodeSynthesisInjectionCls(interf_record,
                                                        self.perm_dict,
                                                        self.handlers_dict,
-                                                       self._read_config_file())
+                                                       self.infrastruc_code_dict,
+                                                       self._read_config_file(),
+                                                       sc_file)
                     walker.walk(tree)
                 # Overwrite source code file to include synthesized code
                 with open(sc_file, mode='w') as sc_file_obj:
@@ -99,12 +125,19 @@ class CodeSynthesisInjectionCls(astor.TreeWalk):
                  interf_record,
                  perm_dict,
                  handlers_dict,
-                 config_dict):
+                 infrastruc_code_dict,
+                 config_dict,
+                 sc_file):
+        """
+        Class constructor.
+        """
         super().__init__()
         self.interf_record = interf_record
         self.perm_dict = perm_dict
         self.handlers_dict = handlers_dict
+        self.infrastruc_code_dict = infrastruc_code_dict
         self.config_dict = config_dict
+        self.sc_file = sc_file
 
     # === Protected Method ===
     def _check_api_permissions(self,
@@ -135,7 +168,7 @@ class CodeSynthesisInjectionCls(astor.TreeWalk):
         """
         Method that checks whether the specified API for a given
         service and a given interface object type (i.e., client
-        or resouce) is present in the configuration file. If so,
+        or resource) is present in the configuration file. If so,
         True is returned, False otherwise.
         """
         try:
@@ -150,9 +183,117 @@ class CodeSynthesisInjectionCls(astor.TreeWalk):
             print(f'--- {e} ---')
             return False
 
+    # === Protected Method ===
+    def _get_handler_qual_name(self, handler_name):
+        """
+        Method that obtains the handler qualified name in preparation
+        for code synthesis. The implementation relies on the dictionary
+        mapping the infrastructure code.
+        """
+        handler_qual_name = self.infrastruc_code_dict['functions'][handler_name]['handler'].split('/')[-1]
+        if handler_qual_name.split('.')[0] == os.path.splitext(os.path.basename(self.sc_file))[0]:
+            # When the above condition is met, the handler to be called is
+            # included in the source code file being processed. Therefore,
+            # prepending the name of the module containing the handler is
+            # not necessary, and it will be removed.
+            return handler_qual_name.split('.')[1]
+        else:
+            return handler_qual_name
+
+    # === Protected Method ===
+    def _get_syn_code(self, service, interf_obj_type, api_name):
+        """
+        Method that implements a generator that yields a tuple with
+        the following elements:
+        1) AST node corresponding to the event object initialization
+        statement. A model for the event object is used, rather than
+        a full initialization.
+        2) AST node corresponding to the synthesized handler call.
+        NOTE: When an exception is raised, this generator yields the
+        tuple (None, None).
+        """
+        # To facilitate the retrieval of the handlers triggered by a
+        # a given event, the handlers dictionary stored in one of the
+        # instance variables is first processed with a function.
+        events_handlers_dict = get_events_handlers_dict(self.handlers_dict)
+        # The logic implemented in this method takes into account that
+        # a given API call can trigger multiple events, and for each of
+        # them code synthesis and injection are potentially needed.
+        for event in self.config_dict[service][interf_obj_type + '_obj'][api_name]['events']:
+            try:
+                for handler in events_handlers_dict[(service, event)]:
+                    handler_qual_name = self._get_handler_qual_name(handler)
+                    # Synthetize event object initialization
+                    event_obj_init_stmt = self._syn_event_obj_init_stmt(service, event)
+                    # Synthetize handler call
+                    syn_handler_call = self._syn_handler_call(handler_qual_name)
+                    yield event_obj_init_stmt, syn_handler_call
+            except:
+                yield None, None
+
+    # === Protected Method ===
+    def _syn_event_obj_init_stmt(self, service, event):
+        """
+        Method that synthesizes and returns the AST node containing
+        the event object initialization.
+        """
+        print('--- Synthesis of the event object initialization is incomplete ---')
+        ast_node = ast.Assign(targets=[ast.Name(id="event", ctx=ast.Store())],
+                              value=ast.Dict([ast.Name('Test', ast.Load())],
+                                             [ast.Name('Test', ast.Load())]))
+        return ast_node
+
+    # === Protected Method ===
+    def _syn_handler_call(self, handler_qual_name):
+        """
+        Method that synthesizes and returns the AST node containing
+        a handler call.
+        """
+        # Depending on whether the qualified handler name passed
+        # as input argument includes a module name, a different
+        # AST node, part of the returned one, is initialized.
+        if '.' in handler_qual_name:
+            handler_func = ast.Attribute(value=ast.Name(id=handler_qual_name.split('.')[0], ctx=ast.Load()),
+                                         attr=handler_qual_name.split('.')[1], ctx=ast.Load())
+        else:
+            handler_func = ast.Name(handler_qual_name, ast.Load())
+        # Arguments of the handler call
+        args = [ast.Name(id='event', ctx=ast.Load()), ast.Name(id='context', ctx=ast.Load())]
+        return ast.Expr(ast.Call(handler_func, args, []))
+
     # === Method ===
     def pre_body_name(self):
-        pass
+        """
+        Method that inserts synthesized code in the tree data structure.
+        """
+        body = self.cur_node
+        # NOTE: Auxiliary counter to consistently modify in place
+        # the tree data structure. This is due to the fact that the
+        # main for loop implemented in this method iterates through
+        # a copy of the list body. The counter is initialized to 1
+        # to ensure that the synthesized code is added after the
+        # relevant API call.
+        counter = 1
+        for i, child in enumerate(body[:]):
+            self.__api_to_process = None
+            # The method pre_Call is automatically called when the
+            # method walk, provided by the base class, is executed.
+            self.walk(child)
+            if self.__api_to_process is not None:
+                for event_obj_init_stmt, syn_handler_call in self._get_syn_code(self.interf_record.service,
+                                                                                self.interf_record.instance_type,
+                                                                                self.__api_to_process):
+                    if all([event_obj_init_stmt is not None, syn_handler_call is not None]):
+                        # Inject synthesized code with initialization of event object
+                        body.insert(i + counter, event_obj_init_stmt)
+                        # Increment auxiliary counter
+                        counter += 1
+                        # Inject synthesized code with handler call
+                        body.insert(i + counter, syn_handler_call)
+                        # Increment auxiliary counter
+                        counter += 1
+        self.__api_to_process = None
+        return True
 
     # === Method ===
     def pre_Call(self):
@@ -166,10 +307,10 @@ class CodeSynthesisInjectionCls(astor.TreeWalk):
         module ast, refer to:
         https://greentreesnakes.readthedocs.io/en/latest/nodes.html#Call
         """
-        # The purpose of this method is to initialize the private
-        # attribute __name. When the initialization is not possible
+        # The purpose of this method is to initialize the following
+        # private attribute. When the initialization is not possible
         # or fails, the default value will remain unchanged.
-        self.__name = None
+        self.__api_to_process = None
         try:
             # Check interface object instance name and if API call is supported
             if all([self.interf_record.instance_name == self.cur_node.func.value.id,
@@ -181,7 +322,9 @@ class CodeSynthesisInjectionCls(astor.TreeWalk):
                 if self._check_api_permissions(self.interf_record.service,
                                                self.interf_record.instance_type,
                                                self.cur_node.func.attr):
-                    print('--- Code synthesis and injection only partially implemented ---')
+                    self.__api_to_process = self.cur_node.func.attr
+                else:
+                    print('--- Permissions for API {self.cur_node.func.attr} not found - No code injected ---')
         except AttributeError:
             # This exception is raised every time the func attibute of
             # the of Call object is not an Attribute object. To avoid
