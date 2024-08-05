fc3237c5be5d5336f08bb9650c0ef06c0750ff67
Giuseppe Raffa <giuseppe.raffa@hotmail.com>
2024-08-05 18:02:00 +0100

    Added tests for function analyse_event_filtering

---
+++ tests/test-files/eventfilteringreslib/httphandler_literal_filter_true.py
@@ -0,0 +1,30 @@
+# ---------------------
+# Import Python Modules
+# ---------------------
+import boto3
+import os
+
+# -------
+# Handler 
+# -------
+def onHTTPPostEvent(event, context):
+    # ---------------------
+    # Retrieve message body
+    # ---------------------
+    msgBodyDict = event['body']
+    # -----------------
+    # Set S3 bucket key
+    # -----------------
+    # Folder within the bucket and name of the uploaded file 
+    uploadFileName = msgBodyDict + '.txt'
+    # --------------------------------------------------------
+    # Set local path (where the file to be uploaded is stored)
+    # --------------------------------------------------------
+    localFile = os.path.join('/tmp', uploadFileName)
+    # --------------------
+    # Call upload_file API
+    # --------------------
+    # Create a boto3 client object instance
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(localFile, os.environ['BUCKET_NAME'], 'uploads/test_file.txt')
+    return
--- tests/test_eventfilteringreslib.py
+++ tests/test_eventfilteringreslib.py
@@ -1,6 +1,7 @@
 # ========================================
 # Import Python Modules (Standard Library)
 # ========================================
+import ast
 import os
 import pytest
 
@@ -8,7 +9,35 @@ import pytest
 # Import Python Modules (Project-specific)
 # ========================================
 from cloudflow.utils.fileprocessingreslib import extract_dict_from_yaml
-from cloudflow.modules.eventfilteringreslib import EventFilteringManagerCls
+from cloudflow.modules.eventfilteringreslib import EventFilteringManagerCls, analyse_event_filtering
+
+# ===================
+# Auxiliary Functions
+# ===================
+def extract_api_call_ast_node(source_code_full_path,
+                              call_func_id='s3_client',
+                              call_attr_value='upload_file'):
+    """
+    Function that extracts and returns the AST node
+    of an API call (ast.Call type) from the source
+    code file specified as input parameter (string).
+    If the AST Call node does not meet the conditions
+    specified by the optional input parameters, None
+    is returned.
+    """
+    with open(source_code_full_path) as sc_file_obj:
+        # Obtain in-memory data structure
+        tree = ast.parse(sc_file_obj.read())
+        # Identify all AST Call nodes
+        for flt_node in (node for node in ast.walk(tree) if isinstance(node, ast.Call)):
+            try:
+                # Identify AST Call nodes that meet specific conditions
+                if all([flt_node.func.value.id == call_func_id,
+                        flt_node.func.attr == call_attr_value]):
+                    return flt_node
+            except:
+                pass
+    return
 
 # ========
 # Fixtures
@@ -50,3 +79,25 @@ def test_source_code_var(get_test_files_folder, handler, var, expected_result):
                                                        sc_file_full_path)
     result = event_filtering_manager.get_event_filtering_result(var, 'unresolved')
     assert result == expected_result
+
+@pytest.mark.parametrize('sc_file, expected_result', [
+    ('httphandler_assign_filter_false.py', False),
+    ('httphandler_assign_filter_true.py', True),
+    ('httphandler_literal_filter_false.py', False),
+    ('httphandler_literal_filter_true.py', True),
+])
+def test_analyse_event_filtering(get_test_files_folder,
+                                 sc_file,
+                                 expected_result):
+    infrastruc_code_dict = extract_dict_from_yaml(get_test_files_folder, 'serverless_s3_event_filtering.yml')
+    # Process source code file
+    sc_file_full_path = os.path.join(get_test_files_folder, sc_file)
+    api_call_ast_node = extract_api_call_ast_node(sc_file_full_path)
+    # Call function under test
+    event_filtering_res = analyse_event_filtering('s3',
+                                                  [{'Key': '2'}],
+                                                  api_call_ast_node,
+                                                  infrastruc_code_dict,
+                                                  'onS3Upload',
+                                                  sc_file_full_path)
+    assert event_filtering_res == expected_result
